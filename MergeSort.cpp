1ca8227fa144e49b3ec334741e4708c5147b380d
studane <zhidaneprabowoguzlend@gmail.com>
2026-03-31 10:50:48 +0700

    menambah panjang array

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -8,4 +8,18 @@ int n;
 
 void input()
 {
+    while (true)
+    {
+        cout << "Masukkan Panjang element array : ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nMaksimal panjang array adalah 20";
+        }
+    }
 }
