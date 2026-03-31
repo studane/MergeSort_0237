5d0015f034e817ceb0205e6ace6c38360673f61f
studane <zhidaneprabowoguzlend@gmail.com>
2026-03-31 11:03:01 +0700

    menyalin merge ke array

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -82,3 +82,10 @@ void mergeSort(int low, int high)
         i++;           // step 4.f.ii
         k++;           // step 4.f.iii
     }
+
+    // step 5
+    for (int x = low; x <= high; x++)
+    {
+        arr[x] = B[x];
+    }
+}
